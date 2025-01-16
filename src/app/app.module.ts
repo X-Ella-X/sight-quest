9a9d70db428ddcc8d51b8b1bfb073aea45e5bdcb
X-Ella-X <e.felsinger@web.de>
2025-01-16 21:27:02 +0100

    create a city list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CityListComponent } from './city-list/city-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/city-list/city-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bm-city-list',
+  templateUrl: './city-list.component.html',
+  styleUrls: ['./city-list.component.css']
+})
+export class CityListComponent {
+
+}
